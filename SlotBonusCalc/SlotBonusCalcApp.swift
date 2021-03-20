3f0a01de39b7e45ee2194e0a6f2356c17e2ae163
YanaPIIDXer <>
2021-03-20 20:13:55 +0900

    プロジェクト生成

---
+++ SlotBonusCalc/SlotBonusCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotBonusCalcApp.swift
+//  SlotBonusCalc
+//
+//  Created by YanaP on 2021/03/20.
+//
+
+import SwiftUI
+
+@main
+struct SlotBonusCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
